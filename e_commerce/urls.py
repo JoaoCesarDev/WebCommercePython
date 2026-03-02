2da50e41151eb7c10e3fc41a6e6e280d61ca0698
João César Rodrigues Ferreira <66702938+JoaoCesarDev@users.noreply.github.com>
2026-03-02 19:32:22 -0300

    construindo a página que vai mostrar os detalhes de cada produtos

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -19,7 +19,7 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from e_commerce.views import home_view, about_page, contact_page, login_page, register_page
-from products.views import ProductListView, product_list_view
+from products.views import ProductListView, product_list_view, ProductDetailView, product_detail_view
 
 urlpatterns = [
     path('',home_view),
@@ -29,6 +29,8 @@ urlpatterns = [
     path('register/', register_page),
     path('products/', ProductListView.as_view()),
     path('products-fbv/', product_list_view),
+    path('products/<int:pk>', ProductDetailView.as_view()),
+    path('products-fbv/<int:pk>', product_detail_view),
     path('admin/', admin.site.urls),
 ]
 if settings.DEBUG:
